88b9d131de3e941409057b5ec797a1e29978f62c
AndrewBalan <balan.andrey.93@gmail.com>
2022-11-02 23:11:42 +0200

    started tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +27,27 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " active";
 }
+
+//  tabs start
+
+function openTab(evt, openTab) {
+
+  let i, tabcontent, tablinks;
+
+  tabcontent = document.querySelectorAll(".tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.querySelectorAll(".tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(openTab).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.querySelector("#defaultOpen").click();
+
+
